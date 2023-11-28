eba7edb72384bbe33124cf27692127f0416ef44d
Nelav <valenntino705@gmail.com>
2023-11-28 00:41:28 -0400

    Agregando las relaciones entre tablas

---
+++ app/Models/Producto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    //use HasFactory;
+
+    public function proveedor()
+    {
+       return $this->belongsTo(Proveedor::class);
+    }
+
+    public function venta()
+    {
+       return $this->belongsTo(Venta::class);
+    }
+}
---
+++ app/Models/Venta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Venta extends Model
+{
+    //use HasFactory;
+    public function productos()
+    {
+       return $this->hasMany(Producto::class);
+    }
+
+    public function cliente()
+    {
+       return $this->belongsTo(Cliente::class);
+    }
+}
