682f1fb668334f7594e65e360269ad3d93115d0c
Nelav <valenntino705@gmail.com>
2023-11-28 11:30:47 -0400

    Agregando la tabla categorias, corrigiendo relaciones de tablas y atributos

--- database/migrations/2023_11_28_041441_create_categorias_table.php
+++ database/migrations/2023_11_28_041441_create_categorias_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('productos', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->integer('stock');
-            $table->date('fechaVencimiento');
-            $table->decimal('precio', 8, 2);
-            $table->foreignId('proveedores_id')->constrained();
-            $table->foreignId('categorias_id')->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('productos');
+        Schema::dropIfExists('categorias');
     }
 };
